818360ddaf6289fb1b4e387bdda80272bb6fa244
voyager <voyager2511@outlook.com>
2017-05-27 17:23:21 +0800

    learn how to code from netty rpc

--- src/main/java/com/nettyrpc/client/RpcFuture.java
+++ src/main/java/com/nettyrpc/client/RpcFuture.java
@@ -1,11 +1,69 @@
 package com.nettyrpc.client;
 
+import com.nettyrpc.protocol.RpcRequest;
+import com.nettyrpc.protocol.RpcResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Description:
  * @Created by Johnny Chou on 2017/5/26.
  * @Author：
  */
-public class RpcFuture {
+public class RpcFuture implements Future<Object> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RpcFuture.class);
+
+    private Sync sync;
+    private RpcRequest request;
+    private RpcResponse response;
+    private long startTime;
+    private long responseTimeThreadshold = 5000;
+    private List<AsyncRpcCallback> pendingCallbacks = new ArrayList<AsyncRpcCallback>();
+    private ReentrantLock lock = new ReentrantLock();
+
+    public RpcFuture(RpcRequest request){
+        this.sync = new Sync();
+        this.request = request;
+        this.startTime = System.currentTimeMillis();
+    }
 
 
+
+
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public boolean isDone() {
+        return sync.isDone();
+    }
+
+    @Override
+    public Object get() throws InterruptedException, ExecutionException {
+        sync.acquire(-1);
+
+        return null;
+    }
+
+    @Override
+    public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
 }
--- src/main/java/com/nettyrpc/client/proxy/ObjectProxy.java
+++ src/main/java/com/nettyrpc/client/proxy/ObjectProxy.java
@@ -3,11 +3,13 @@ package com.nettyrpc.client.proxy;
 import com.nettyrpc.client.ConnectionManage;
 import com.nettyrpc.client.RpcClientHandler;
 import com.nettyrpc.client.RpcFuture;
+import com.nettyrpc.protocol.RpcRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.UUID;
 
 /**
  * @Description:
@@ -26,6 +28,33 @@ public class ObjectProxy<T> implements InvocationHandler,IAsyncObjectProxy {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        if(Object.class == method.getDeclaringClass()){
+            String name = method.getName();
+            if("equals".equals(name)){
+                return proxy = args[0];
+            }else if("hashCode".equals(name)){
+                return System.identityHashCode(proxy);
+            }else if("toString".equals(name)){
+                return proxy.getClass().getName() + "@"+Integer.toHexString(System.identityHashCode(proxy))+",with InvocationHandler"+this;
+            }else{
+                throw new IllegalStateException(String.valueOf(method));
+            }
+
+        }
+
+        RpcRequest request = new RpcRequest();
+        request.setRequestId(UUID.randomUUID().toString());
+        request.setClassName(method.getDeclaringClass().getName());
+        request.setMethodName(method.getName());
+        request.setParameterType(method.getParameterTypes());
+        request.setParameters(args);
+        //debug
+        logger.debug(method.getDeclaringClass().getName());
+
+
+
+
         return null;
     }
 
